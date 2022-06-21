84a8a4521f8083232168a368b03d5b160144dc83
lluisllc <lluis.llacera@gmail.com>
2022-06-21 15:22:27 +0200

    Todas las ventanas funcionan
    tb las fotos

--- app.js
+++ app.js
@@ -7,11 +7,17 @@ const app = express();
 // Make everything inside of public/ available
 app.use(express.static('public'));
 
-// our first Route:
-app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home-page.html'));
+// primera ruta
+app.get('/', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
 
-// cat route:
-app.get('/cat', (request, response, next) => response.sendFile(__dirname + '/views/cat-page.html'));
+//about
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+//works
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+//gallery
+app.get('/gallery', (request, response, next) => response.sendFile(__dirname + '/views/gallery.html'));
 
 // Server Started
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
